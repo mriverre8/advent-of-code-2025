53c1796a1a6077ea28a672158adec9ea7a2e1c94
mriverre8 <rivemark00@gmail.com>
2025-12-26 14:09:02 +0100

    day 5 solution

--- day_5/part-1.js
+++ day_5/part-1.js
@@ -3,21 +3,10 @@ const fs = require("fs");
 const content = fs.readFileSync("input.txt", "utf8");
 const lines = content.split(/\r?\n/);
 
-let isGeneratingRanges = true;
-let ranges = [];
 let answer = 0;
 
-const checkIfIDsInRange = (line, ranges) => {
-  for (let i = 0; i < ranges.length; i++) {
-    const min = parseInt(ranges[i].split("-")[0], 10);
-    const max = parseInt(ranges[i].split("-")[1], 10);
-    const id = parseInt(line, 10);
-    if (id >= min && id <= max) {
-      answer += 1;
-      return;
-    }
-  }
-};
+let isGeneratingRanges = true;
+let ranges = [];
 
 lines.forEach((line) => {
   if (line === "") {
@@ -27,7 +16,14 @@ lines.forEach((line) => {
   if (isGeneratingRanges) {
     ranges.push(line);
   } else {
-    checkIfIDsInRange(line, ranges);
+    for (let i = 0; i < ranges.length; i++) {
+      const min = parseInt(ranges[i].split("-")[0], 10);
+      const max = parseInt(ranges[i].split("-")[1], 10);
+      const id = parseInt(line, 10);
+      if (id >= min && id <= max) {
+        return (answer += 1);
+      }
+    }
   }
 });
 
